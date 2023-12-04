93f918901678eb9e596b8f63bffeb00f04131d28
Therese Chang <56442073+erryte@users.noreply.github.com>
2023-12-04 12:02:39 -0800

    Update import_sqlite3.py
    trying to create a PR but need something changed so code is different from the main branch

--- import_sqlite3.py
+++ import_sqlite3.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+# add line to have change in order to initiate a PR
 conn = sqlite3.connect('database.db')
 conn.execute('CREATE TABLE accounts (username TEXT, password TEXT)')
 conn.close()
